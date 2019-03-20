7470edc61e218ed20932a60bee023dfda1705d4c
huhuhang <huhuhang@gmail.com>
2019-03-20 12:23:37 +0800

    add

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
+	load_model() # 提前加载模型
 	data = {"success": False}
 	if flask.request.method == "POST":
 		if flask.request.files.get("image"):
@@ -59,5 +60,4 @@ def predict():
 
 # 启动
 if __name__ == "__main__":
-	load_model() # 提前加载模型
 	app.run(host='127.0.0.1', debug=True)
